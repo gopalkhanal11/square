7b9b74c0edbbe9fbc5cfac957613129d56c26197
gopalkhanal11 <khanal.joshipur@gmail.com>
2023-11-02 11:14:54 -0700

    making vignettes and website

---
+++ R/patients.R
@@ -0,0 +1,13 @@
+#' World Health Organization TB data
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `patients`
+#' A data frame with 3 rows and 2 columns:
+#' \describe{
+#'   \item{id}{ID}
+#'   \item{age}{AGE}
+#'   ...
+#' }
+"patients"
